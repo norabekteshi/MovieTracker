b290112d3236923a3bb8d9c63ce8c903d1b1f48a
norabekteshi <norabekteeshi@gmail.com>
2026-05-18 15:42:53 +0200

    Initial Commit

---
+++ Assignment 2 NE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 2 NE
+//
+//  Created by Nora Bekteshi on 18.5.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
