37b481386e5566a2e1efca70528cce3718eb27cb
norabekteshi <norabekteeshi@gmail.com>
2026-05-20 17:34:24 +0200

    Starting fresh

--- Assignment 2 NE/UIViewController+Alerts.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-extension UIViewController {
-    func showSimpleAlert(title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default))
-        present(alert, animated: true)
-    }
-}
